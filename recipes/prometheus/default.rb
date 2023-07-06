b16072fcb3cb80ec7e034fdc1cbfb272939e3eaa
tosuke <mail@tosuke.me>
2023-07-06 13:42:02 +0900

    feat: restart prometheus on config updates

--- recipes/prometheus/default.rb
+++ recipes/prometheus/default.rb
@@ -6,4 +6,10 @@ end
 
 remote_file "/config/prometheus/prometheus.yml" do
   mode "644"
+  notifies :run, "execute[restart container prometheus]"
+end
+
+execute "restart container prometheus" do
+  action :nothing
+  command "/bin/vbash -c 'source /opt/vyatta/etc/functions/script-template; vyatta_cfg_run run restart container prometheus'"
 end
