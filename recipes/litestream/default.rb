ac551f9ae034952df317ac09fde6ca7708287442
tosuke <mail@tosuke.me>
2023-07-08 18:46:18 +0900

    feat: litestream metrics

--- recipes/litestream/default.rb
+++ recipes/litestream/default.rb
@@ -2,4 +2,10 @@ directory "/config/litestream"
 
 remote_file "/config/litestream/litestream.yml" do
   mode "644"
+  notifies :run, "execute[restart container litestream]"
+end
+
+execute "restart container litestream" do
+  action :nothing
+  command "/bin/vbash -c 'source /opt/vyatta/etc/functions/script-template; vyatta_cfg_run run restart container litestream'"
 end
